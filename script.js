9867267d8f180c760a81ac53f56146aa06a4ee8e
Nina Maxberry <nmax@DESKTOP-R9KQV1C.nmaxberry@yahoo.com>
2019-03-24 15:56:53 -0400

    Updates 032419

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
+/* Thank the visitor for showing their interest */
 function myThanks() {
-    var you = document.getElementById('fname').value;
-    alert("Thank you " + you + " for your interest. We will contact you soon.");
-
+    alert("Thank you " + fname.value + " for your interest.  We will contact you soon.");
+    
 }
+           
+
+/* Open email dialog box directly to our email */
+
+function myMail() {
+    const maiLink = document.getElementById("mailtoLink");
+    new MailToLink(mailLink);
+}
+
